908308e16737af59f053cfa2ba52440b1d6b4e65
mirai-1213 <47546555+mirai-1213@users.noreply.github.com>
2019-02-12 14:56:43 +0900

    Add files via upload

--- text1.py
+++ text1.py
@@ -1,2 +1,12 @@
-score = int(input())
-print ("GREAT!" if score > 80 else "SO SO!..") 
+i = 3
+k = i
+while i>0:
+    j=4-i
+#    while k>1:
+#        print(" ",end="")
+#        k-=1
+    while j>0:
+        print("*",end="")
+        j-=1
+    print('')
+    i-=1;
